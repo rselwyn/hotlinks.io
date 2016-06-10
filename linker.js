d0ce99eea4c88ab9a96f12157d15e40b39ffa09e
Robbie S <robbie@selwynsoftware.com>
2016-06-10 12:14:14 -0700

    Image shows and removes depending on input

--- linker.js
+++ linker.js
@@ -1,25 +1,49 @@
 var inNewWindow = true
+var outgoingUrl = ""
 
 $(function(){
+	showLogoIn()
 	$("#maininput").keyup(function (e) {
+		doInput()
 	    if (e.keyCode == 13) {
-	       	doInput()
+	       	if (outgoingUrl!= "") goToUrl()
 	    }
 	});
 });
 
+window.setInterval(function(){
+  showLogoIn()
+}, 500);
+
 function doInput() {
-
-	var outgoingUrl = "http://www."
-
 	var value = $("#maininput").val()
 	var splitted = value.split("")
 	if (splitted[0] == "@") {
-		var newText = "";
-		newText = splitted.slice(1,splitted.length).join("")
-		outgoingUrl+="twitter.com/" + newText
+		var newText = splitted.slice(1,splitted.length).join("")
+		outgoingUrl="http://www.twitter.com/" + newText
 	}
+	else if (splitted[0] == "_") {
+		outgoingUrl += "http://www.facebook.com/" + splitted.slice(1,splitted.length).join("")
+	}
+	else {
+		outgoingUrl = ""
+	}
+}
 
+function showLogoIn () {
+	if (outgoingUrl=="") {
+		console.log("Reset")
+		document.getElementsByClassName("simage")[0].style.display = "none"
+		document.getElementById("maininput").style.margin = "auto"
+	}
+	else {
+		console.log("Change")
+		document.getElementsByClassName("simage")[0].style.display = "inline"
+		document.getElementById("maininput").style.marginRight = "20px"
+	}
+}
+
+function goToUrl() {
 	if (!inNewWindow) {
 		window.location.href = outgoingUrl
 	}
