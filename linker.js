2d52603290c2107b123141d69c34b699b90a35b7
Robbie S <robbie@selwynsoftware.com>
2016-06-10 14:17:40 -0700

    Added Yahoo Finance support

--- linker.js
+++ linker.js
@@ -5,7 +5,8 @@ var site = ""
 var siteImageMap = {
 	facebook: "https://pbs.twimg.com/profile_images/3513354941/24aaffa670e634a7da9a087bfa83abe6_200x200.png",
 	twitter: "https://g.twimg.com/Twitter_logo_blue.png",
-	github: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/PEO-octocat-2.svg/1000px-PEO-octocat-2.svg.png"
+	github: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/PEO-octocat-2.svg/1000px-PEO-octocat-2.svg.png",
+	yahoofinance: "https://lh5.ggpht.com/dTFTNAPcPpdd52VjidAJX7N8WYS6KuZ01l2NoXHF01iSTzLZLJ5ng-HEeuy8vCYL5DU=w300"
 }
 
 $(function(){
@@ -37,6 +38,10 @@ function doInput() {
 	else if (splitted[0] + splitted[1] == "gh") {
 		outgoingUrl = "https://www.github.com/"
 		site = "github"
+	}
+	else if (splitted[0] == "$" && splitted.length > 1) {
+		outgoingUrl = "http://www.finance.yahoo.com/q?s="+splitted.slice(1,splitted.length).join("")
+		site = "yahoofinance"
 	}
 	else {
 		outgoingUrl = ""
